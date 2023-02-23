3a75b7b920a33e2c0eaf4e30ce189e6d94aab9ac
KasperKopke <kasper@kopke.info>
2023-02-23 21:22:53 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let userName = "Kasper";
+let userAge = "18";
+
+if (userName == "Kasper") {
+  console.log("Hej Kasper din seje reje");
+} else {
+  console.log("du hedder ikke Kasper");
+}
+
+if (userAge >= 19) {
+  console.log("du er ikke gammel nok din lille lort");
+} else {
+  console.log("du er gammel nok din store lort");
+}
